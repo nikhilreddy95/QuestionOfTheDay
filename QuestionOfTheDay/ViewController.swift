58292d773dc7e95b45aa5d5c142190115f9bec0c
student <s528741@ch140003m.local>
2017-11-06 17:57:21 -0600

    First Commit

---
+++ QuestionOfTheDay/ViewController.swift
@@ -0,0 +1,35 @@
+ import UIKit
+
+class ViewController: UIViewController {
+    
+    let APPLICATION_ID = ""
+    let API_KEY = ""
+    let SERVER_URL = "https://api.backendless.com"
+    let backendless = Backendless.sharedInstance()!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        backendless.hostURL = SERVER_URL
+        backendless.initApp(APPLICATION_ID, apiKey: API_KEY)
+        
+        // Saving test object in the test table
+        let testObject = ["foo" : "bar"];
+        let dataStore = backendless.data.ofTable("TestTable")
+        dataStore?.save(testObject,
+                        response: {
+                            (result) -> () in
+                            print("Object is saved in Backendless. Please check in the console.")
+        },
+                        error: {
+                            (fault : Fault?) -> () in
+                            print("Server reported an error: \(String(describing: fault))")
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+        
